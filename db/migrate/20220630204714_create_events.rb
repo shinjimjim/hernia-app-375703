e9df1c191cdc99277b4c61fc921f466d74541957
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-07-01 05:53:07 +0900

    Eventモデルの記述

--- db/migrate/20220630204714_create_events.rb
+++ db/migrate/20220630204714_create_events.rb
@@ -1,6 +1,15 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
+      t.integer    :pain_id,    null: false
+      t.integer    :stretch_id
+      t.integer    :training_id
+      t.integer    :posture_id
+      t.integer    :meal_id
+      t.integer    :motion_id
+      t.integer    :smoking_id
+      t.text       :postscript
+      t.datetime   :start_time
 
       t.timestamps
     end
