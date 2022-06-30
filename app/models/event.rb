e3728911511a9a82448d938fc61cb211170daec7
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-07-01 06:02:44 +0900

    Eventモデルの編集

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,12 @@
 class Event < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :pain
+  belongs_to :stretch
+  belongs_to :training
+  belongs_to :posture
+  belongs_to :meal
+  belongs_to :motion
+  belongs_to :smoking
+
+  validates :pain_id, numericality: { other_than: 0, message: "can't be blank" }
 end
