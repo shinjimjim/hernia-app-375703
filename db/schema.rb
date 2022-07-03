df3a7cb74092918865e68c5310d7fd39c725c17e
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-07-03 15:39:02 +0900

    Eventsテーブルへのuse_idの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_043730) do
+ActiveRecord::Schema.define(version: 2022_07_03_062321) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "pain_id", null: false
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2022_07_03_043730) do
     t.datetime "start_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_07_03_043730) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
