32b9aad6b88a964566a8191fbee5eefb54fa4dfa
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-07-05 08:02:03 +0900

    adminのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
-  resource :user, only: [:show, :edit, :update] do
+  resource :user, only: [:show, :edit, :update] do 
     resources :events
+      namespace :admin do
+        resources :users, only: [:index, :show]
+      end
   end
-  end
+end
