a15987fbfbe7e0fc655f3a01272f7b2ffdf60224
shinjimjim <erics6lihin8em9oics@gmail.com>
2022-07-05 07:47:14 +0900

    Usersテーブルに管理者情報を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_062321) do
+ActiveRecord::Schema.define(version: 2022_07_04_224118) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "pain_id", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_07_03_062321) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
